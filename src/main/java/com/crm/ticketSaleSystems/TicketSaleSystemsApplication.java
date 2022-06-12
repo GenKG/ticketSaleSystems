e476ca751f1aef9e18e4904d1dce29396f24c1ce
Egor Safonov <safon0306@gmail.com>
2022-06-13 01:11:06 +0300

    Initial commit

---
+++ src/main/java/com/crm/ticketSaleSystems/TicketSaleSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.crm.ticketSaleSystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSaleSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSaleSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/ticketSaleSystems/TicketSaleSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.ticketSaleSystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSaleSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
